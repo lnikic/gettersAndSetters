fd7c44f16808c228963fab52ab2f907afa2c62ac
nikic <nikic.luka2001@gmail.com>
2025-11-06 19:38:59 +0100

    Implement Main class to illustrate instance vs local variables

---
+++ src/ElectricalGuitar.java
@@ -0,0 +1,29 @@
+public class ElectricalGuitar {
+    String brand;
+    int numOfPickups;
+    boolean rockStarUsesIt;
+
+    String getBrand() {
+        return brand;
+    }
+
+    void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    int getNumOfPickups() {
+        return numOfPickups;
+    }
+
+    void setNumOfPickups(int numOfPickups) {
+        this.numOfPickups = numOfPickups;
+    }
+
+    boolean isRockStarUsesIt() {
+        return rockStarUsesIt;
+    }
+
+    void setRockStarUsesIt(boolean rockStarUsesIt) {
+        this.rockStarUsesIt = rockStarUsesIt;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
+    int counter = 0;
 
+    public static void main(String[] args) {
+        int count = 0;
+        Main[] mixes = new Main[20];
+        int i = 0;
+        while (i < 19) {
+            mixes[i] = new Main();
+            mixes[i].counter = mixes[i].counter + 1;
+            count = count + 1;
+            count = count + mixes[i].maybeNew(i);
+            i = i + 1;
+        }
+        System.out.println(count + " " +
+                mixes[1].counter);
+    }
+
+    public int maybeNew(int index) {
+        if (index < 1) {
+            Main mix = new Main();
+            mix.counter = mix.counter + 1;
+            return 1;
+        }
+        return 0;
     }
 }
